4267a68d47bb2690c44c366fd87c1c5298dadbb1
quader864 <quader864@users.noreply.github.com>
2025-12-14 23:47:21 +0330

    Deploy 2025-12-14 23:47:21

--- assets/chevron-right-gCO0VDBm.js
+++
@@ -1,6 +0,0 @@
-import{w as o}from"./index-B8C5BywF.js";/**
- * @license lucide-react v0.555.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],c=o("chevron-right",t);export{c as C};
